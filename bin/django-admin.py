234f60cd9bf65315e6d92471936e0a40c8849738
miczthemike <panedmichaeljoseph@gmail.com>
2018-07-18 07:13:04 +0800

    Second commit

---
+++ bin/django-admin.py
@@ -0,0 +1,7 @@
+#!/home/noc/miketestimg/mike/bin/python3
+# EASY-INSTALL-DEV-SCRIPT: 'Django==2.2.dev20180716182847','django-admin.py'
+__requires__ = 'Django==2.2.dev20180716182847'
+__import__('pkg_resources').require('Django==2.2.dev20180716182847')
+__file__ = '/home/noc/miketestimg/mike/bin/django/django/bin/django-admin.py'
+with open(__file__) as f:
+    exec(compile(f.read(), __file__, 'exec'))
